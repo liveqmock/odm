1c76ba93a4c4a7a04348f42ad11839f79503d51a
ailinty <ailinty@163.c>
2014-01-30 10:12:23 +0800

    提交2个文件
    asdfasdfasdf

---
+++ src/com/jiang/timertask/OrderTimerTask.java
@@ -0,0 +1,15 @@
+package com.jiang.timertask;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-10-20
+ * Time: 下午6:34
+ * 定时处理未处理的订单
+ */
+public class OrderTimerTask {
+    public void execute(){
+        System.out.println("Task is executed.");
+    }
+
+}
---
+++ src/com/jiang/timertask/StartUpTimerTask.java
@@ -0,0 +1,14 @@
+package com.jiang.timertask;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-10-20
+ * Time: 下午7:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartUpTimerTask {
+    public void execute(){
+        System.out.println("StartUpTimerTask is executed.");
+    }
+}
